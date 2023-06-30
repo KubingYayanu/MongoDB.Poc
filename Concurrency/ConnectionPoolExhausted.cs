b8324d84886720701510f1f6d8fd2a624fce93d9
Kubing Lin <edkt973@gmail.com>
2023-06-30 16:52:02 +0800

    feat: 新增 ConnectionPoolExhausted 測試

---
+++ Concurrency/ConnectionPoolExhausted.cs
@@ -0,0 +1,33 @@
+﻿using MongoDB.Driver;
+
+namespace MongodB.Poc.Concurrency
+{
+    public class ConnectionPoolExhausted
+    {
+        private const string MemberId = "649d39e57a838f1a21139aef";
+        
+        private int count;
+        
+        public async Task GetMember()
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < 1000; i++)
+            {
+                var task = Task.Run(async () =>
+                {
+                    var client = new MongoClient("mongodb://localhost:2000");
+                    var database = client.GetDatabase("concurrency");
+                    var member = database.GetCollection<Member>("member");
+                    var members = await member.Find(x => x.Id == MemberId)
+                        .ToListAsync();
+                    count += 1;
+                    Console.WriteLine($"Get Member count: {members.Count}.");
+                });
+                tasks.Add(task);
+            }
+
+            await Task.WhenAll(tasks);
+            Console.WriteLine($"Count: {count}.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,16 @@
-﻿namespace MongodB.Poc.Gifts // Note: actual namespace depends on the project name.
+﻿using MongodB.Poc.Concurrency;
+
+namespace MongodB.Poc.Gifts // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
         static async Task Main(string[] args)
+        {
+            await ConnectionPoolExhausted();
+            Console.ReadLine();
+        }
+
+        private static async Task DocumentAtomic()
         {
             var documentAtomic = new DocumentAtomic();
             // await documentAtomic.Version1();
@@ -10,8 +18,12 @@
             // await documentAtomic.Version3();
             // await documentAtomic.Version4();
             await documentAtomic.Version5();
+        }
 
-            Console.ReadLine();
+        private static async Task ConnectionPoolExhausted()
+        {
+            var connections = new ConnectionPoolExhausted();
+            await connections.GetMember();
         }
     }
 }
